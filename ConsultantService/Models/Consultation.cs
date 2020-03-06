f8c334b42e32ace3a7c570e977e45d60e0d46864
Yurij2015 <nabor_2008@i.ua>
2020-03-06 22:10:49 +0300

    Add project files.

---
+++ ConsultantService/Models/Consultation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ConsultantService.Models
+{
+    public class Consultation
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
